b940c74f6fa409bf492557bf8953c174ea398857
Brandon <blor15@hotmail.com>
2023-02-09 20:59:38 -0700

    simple get route created

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+//listen for the app on whatever port
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
