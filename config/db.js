70ef56c1c92ea8fd772268d7443bd497d8b764b9
Brandon <blor15@hotmail.com>
2023-02-12 11:46:31 -0700

    variables created for endpoints

---
+++ config/db.js
@@ -0,0 +1,27 @@
+const envelopes = [
+  {
+    id: 1,
+    title: "Insurance",
+    budget: 500,
+  },
+  {
+    id: 2,
+    title: "Entertainment",
+    budget: 400,
+  },
+  {
+    id: 3,
+    title: "Groceries",
+    budget: 400,
+  },
+  {
+    id: 4,
+    title: "Car payment",
+    budget: 400,
+  },
+  {
+    id: 5,
+    title: "Loans",
+    budget: 1500,
+  },
+];
